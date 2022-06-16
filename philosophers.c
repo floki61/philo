da1e4ba2073dce3844f7d30b6b0655bbd16ad818
oel-berh <oel-berh@student.1337.ma>
2022-06-16 02:24:02 +0100

    1/2

--- philosophers.c
+++ philosophers.c
@@ -6,29 +6,28 @@ void print(t_philo *philo, int i)
 	pthread_mutex_lock(&philo->data->print);
 	// printf("%ld %ld %d\n",gettime(),philo->data->start_philo, philo->id);
 	// printf("philo num = %d\n",philo->id);
-	time = gettime() - philo->data->start_philo;
+	time = (gettime() - philo->data->start_philo) / 1000;
 
 	// printf("time %ld\n", time);
 	// printf("gettime %ld\n", gettime());
 	// printf("start philo %ld\n", philo->data->start_philo);
 
 	if(i == 0)
-		printf("%ld %d has taken a fork\n", time, philo->id);
+		printf("%ld %d has taken a fork\n", time, philo->id + 1);
 	else if(i == 1)
-		printf("%ld %d is eating\n", time, philo->id);
+		printf("%ld %d is eating\n", time, philo->id + 1);
 	else if(i == 2)
-		printf("%ld %d is sleeping\n", time, philo->id);
+		printf("%ld %d is sleeping\n", time, philo->id + 1);
 	else if(i == 3)
-		printf("%ld %d is thinking\n", time, philo->id);
+		printf("%ld %d is thinking\n", time, philo->id + 1);
 	else if(i == 4)
-		printf("%ld %d is died\n", time, philo->id);
+		printf("%ld %d is died\n", time, philo->id + 1);
 	pthread_mutex_unlock(&philo->data->print);
 }
 
 void	*routine(void *philo)
 {
 	t_philo  *ph = (t_philo *)philo;
-
 	while(1)
 	{
 		// printf("philo num = %d\n",ph->id);
@@ -48,25 +47,19 @@ int	creat_philo(t_data *data, t_philo *philo)
 
 	i = 0;
 	i2 = 0;
-	// data->start_philo = gettime();
-	// while(i2 < 2)
-	// {
-	// 	i = i2;
-		while(i < data->n_philo)
-		{
-			philo[i].id = i + 1; 
-			philo[i].start_philo = gettime();
-			philo[i].num_of_meals = 0;
-			philo[i].last_meal = gettime();
-			philo[i].data = data;
-			pthread_create(&data->th[i], NULL, routine, &philo[i]);
-			// printf("id in creation: %d\n", philo[i].id);
-			usleep(100);
-			i += 1;
-		}
-		// if (i2 == 0)
-		// i2++;
-	// }
+	data->start_philo = gettime();
+	while(i < data->n_philo)
+	{
+		philo[i].id = i; 
+		// philo[i].start_philo = gettime();
+		philo[i].num_of_meals = 0;
+		philo[i].last_meal = gettime();
+		philo[i].data = data;
+		pthread_create(&data->th[i], NULL, routine, &philo[i]);
+		// printf("id in creation: %d\n", philo[i].id);
+		usleep(100);
+		i++;
+	}
 	return (0);
 }
 
@@ -96,11 +89,10 @@ int	check_died(t_data *data, t_philo *philo)
 		i = -1;
 		while(++i < data->n_philo)
 		{
-			if(gettime() - (philo[i].last_meal) >= data->t_die)
+			if((gettime() - philo[i].last_meal) / 1000 >= data->t_die)
 			{
-				printf("died\n");
 				pthread_mutex_lock(&philo->data->print);
-		 		printf("%ld %d is died\n", gettime() - philo[i].data->start_philo, philo[i].id);
+		 		printf("%ld %d is died\n", (gettime() - philo[i].data->start_philo) / 1000, philo[i].id + 1);
 				return (1);
 			}
 		}
--- tasks.c
+++ tasks.c
@@ -1,12 +1,12 @@
 #include "philosophers.h"
 
-// size_t gettime()
+// long gettime()
 // {
 // 	struct timeval time;
 // 	long time_usec;
 
 // 	gettimeofday(&time, NULL);
-// 	time_usec = (time.tv_sec * 1000000) + (time.tv_usec / 1000);
+// 	time_usec = (time.tv_sec * 1000) + (time.tv_usec / 1000);
 // 	return (time_usec);
 // }
 long	gettime(void)
@@ -21,18 +21,19 @@ void sleepi(unsigned long t_sleep)
 	unsigned long	start_time;
 
 	start_time = gettime();
-	usleep(t_sleep - 1000);
-	while (((gettime() - start_time) * 1000) < t_sleep);
+	// usleep(t_sleep - 1000);
+	while (((gettime() - start_time)) < t_sleep);
 }
 
 void    take_a_fork(t_philo *philo)
 {
 	// printf("hey from thread %d\n",philo->id - 1);
 	// printf("get time llwla : %ld\n", gettime());
-	pthread_mutex_lock(&philo->data->fork[philo->id - 1]);
+	printf(" philo number %d is waiting\n", philo->id);
+	pthread_mutex_lock(&philo->data->fork[philo->id]);
 	// printf("hey from thread %d %d\n",philo->id - 1 ,philo->id  % philo->data->n_philo);
 	print(philo, 0);
-	pthread_mutex_lock(&philo->data->fork[philo->id % philo->data->n_philo]);
+	pthread_mutex_lock(&philo->data->fork[(philo->id + 1) % philo->data->n_philo]);
 	print(philo, 0);
 }
 
@@ -43,8 +44,9 @@ void	eating(t_philo *philo)
 	sleepi(philo->data->t_eat * 1000);
 	philo->num_of_meals += 1;
 	// printf("%d num of meals == %d\n",philo->id, philo->num_of_meals);
-	pthread_mutex_unlock(&philo->data->fork[philo->id - 1]);
-	pthread_mutex_unlock(&philo->data->fork[philo->id % philo->data->n_philo]);
+	pthread_mutex_unlock(&philo->data->fork[(philo->id +  1) % philo->data->n_philo]);
+	pthread_mutex_unlock(&philo->data->fork[philo->id]);
+	printf("unlock  %d %d\n",philo->id,(philo->id + 1) % philo->data->n_philo);
 }
 
 void	sleeping(t_philo *philo)
